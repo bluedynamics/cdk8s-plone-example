dc0bdf6a673039c55fd52aae78b8335329817a38
Jens W. Klein <jk@kleinundpartner.at>
2024-07-27 16:28:22 +0200

    example uses zalando postgres operator

--- main.bitnami.ts
+++ main.bitnami.ts
@@ -1,8 +1,7 @@
 import { Construct } from 'constructs';
-import { App, Chart, ChartProps } from 'cdk8s';
+import { App, Chart, ChartProps, Helm } from 'cdk8s';
 import { Plone } from '@bluedynamics/cdk8s-plone';
 import * as kplus from 'cdk8s-plus-24';
-import * as pgop from './imports/acid.zalan.do'
 
 export class ExampleChart extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps = {}) {
@@ -16,18 +15,40 @@ export class ExampleChart extends Chart {
 
     var dbServiceName = 'localhost';
 
-    new pgop.Postgresql(
-      this,
-      'db',
-      {
-        spec: {
-          numberOfInstances: 2,
-          teamId: 'plone',
-          postgresql: { version: pgop.PostgresqlSpecPostgresqlVersion.VALUE_16 },
-          volume: { size: '10Gi' },
+    const db = new Helm(this, 'db', {
+      chart: 'bitnami/postgresql',
+      // TODO: figure out why repo: 'https://charts.bitnami.com/bitnami', does not work
+      // in fact I do not want a namespace here. I want to use the passed in with kubectl apply.
+      // need to figure out how to achieve this
+      namespace: 'plone',
+      values: {
+        'commonLabels': { 'app.kubernetes.io/part-of': 'plone' },
+        'global': {
+          'postgresql': {
+            'postgresPassword': 'admin@postgres',
+            'username': dbuser,
+            'password': dbpass,
+            'database': dbname,
+          },
+        },
+        'auth': {
+          'username': dbuser,
+          'password': dbpass,
+          'database': dbname,
         }
       }
-    );
+    });
+
+    const dbService = db.apiObjects.find(construct => {
+      if ((construct.kind === 'Service') && (construct.metadata.name?.endsWith('postgresql'))) {
+        return construct.name;
+      }
+      return undefined;
+    });
+    if (dbService === undefined) {
+      throw new Error('Could not find postgresql service');
+    }
+    dbServiceName = dbService.name;
 
     const env = new kplus.Env(
       [],
