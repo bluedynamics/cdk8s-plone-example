173002d5cfe5270800d6e677ecbd023bcf84c79a
Jens W. Klein <jk@kleinundpartner.at>
2024-07-28 19:19:47 +0200

    configure with zalando pgoperator

--- main.ts
+++ main.ts
@@ -8,36 +8,68 @@ export class ExampleChart extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps = {}) {
     super(scope, id, props);
 
-    const dbname = 'plone';
-    const dbuser = 'plone';
-    const dbpass = 'admin@plone';
     const mximageversion_backend = 'sha-94c84dc';
     const mximageversion_frontend = 'sha-ffd2b1e';
 
-    var dbServiceName = 'localhost';
-
-    new pgop.Postgresql(
+    const db = new pgop.Postgresql(
       this,
       'db',
       {
+        metadata: {
+          labels: {
+            'app.kubernetes.io/name': 'plone-postgresql',
+            'app.kubernetes.io/instance': 'postgresql',
+            'app.kubernetes.io/component': 'database',
+            'app.kubernetes.io/part-of': 'plone',
+          }
+        },
         spec: {
           numberOfInstances: 2,
+          allowedSourceRanges: ['10.0.0.0/8'],
+          postgresql: { version: pgop.PostgresqlSpecPostgresqlVersion.VALUE_15 },
+          volume: { size: '1Gi' },
+          databases: {
+            plone: 'plone',
+          },
           teamId: 'plone',
-          postgresql: { version: pgop.PostgresqlSpecPostgresqlVersion.VALUE_16 },
-          volume: { size: '10Gi' },
+          users: {
+            plone: [
+              pgop.PostgresqlSpecUsers.SUPERUSER,
+              pgop.PostgresqlSpecUsers.CREATEDB,
+            ],
+          },
+          resources: {
+            limits: {
+              cpu: '1000m',
+              memory: '1Gi',
+            },
+            requests: {
+              cpu: '300m',
+              memory: '400Mi',
+            },
+          },
         }
       }
     );
-
+    // const dbService = db.node.find(construct => {
+    //   log(construct.metadata.name);
+    //   if ((construct.kind === 'Service') && (construct.metadata.name?.endsWith('postgresql'))) {
+    //     return construct.name;
+    //   }
+    //   return undefined;
+    // });
+    const dbMDName = db.metadata.name;
     const env = new kplus.Env(
       [],
       {
+        SECRET_POSTGRESQL_USERNAME: { valueFrom: { secretKeyRef: { name: `plone.${dbMDName}.credentials.postgresql.acid.zalan.do`, key: 'username' }}},
+        SECRET_POSTGRESQL_PASSWORD: { valueFrom: { secretKeyRef: { name: `plone.${dbMDName}.credentials.postgresql.acid.zalan.do`, key: 'password' }}},
         INSTANCE_db_storage: { value: `relstorage` },
         INSTANCE_db_blob_mode: { value: `cache` },
         INSTANCE_db_cache_size: { value: `5000` },
         INSTANCE_db_cache_size_bytes: { value: `1500MB` },
         INSTANCE_db_relstorage: { value: `postgresql` },
-        INSTANCE_db_relstorage_postgresql_dsn: { value: `host='${dbServiceName}' dbname='${dbname}' user='${dbuser}' password='${dbpass}'` },
+        INSTANCE_db_relstorage_postgresql_dsn: { value: `host='${dbMDName}' dbname='plone' user='$(SECRET_POSTGRESQL_USERNAME)' password='$(SECRET_POSTGRESQL_PASSWORD)'` },
         INSTANCE_db_relstorage_cache_local_mb: { value: `800` },
       },
     );
